0021ae82b45d5b2fbbb9acb63d1a85ce62236177
Thomas Sigler <ts309435@gmail.com>
2025-09-01 14:07:30 -0500

    boss two pattern two working

--- src/entity/enemy/boss/BossTwo.cs
+++ src/entity/enemy/boss/BossTwo.cs
@@ -18,6 +18,7 @@ public partial class BossTwo : Sprite2D
     private Vector2 Viewport;
     private int SideChoice = 1;
 
+    [Export]
     private int stage = 0;
     private double deltaAccumulate;
 
@@ -52,7 +53,32 @@ public partial class BossTwo : Sprite2D
 
         BulletPatterns[1] = (Random rng, double speed, int d) =>
         {
-            var bullet = new BossTwoBulletOne();
+            var bullet = new BossTwoBulletTwo();
+
+            double StartPlace = rng.NextDouble();
+            if (StartPlace < 0.25)
+                bullet.Position = new Vector2((float)(rng.NextDouble() * Viewport.X), 0.0f);
+            else if (StartPlace < 0.5)
+                bullet.Position = new Vector2((float)(rng.NextDouble() * Viewport.X), Viewport.Y);
+            else if (StartPlace < 0.75)
+                bullet.Position = new Vector2(0.0f, (float)(rng.NextDouble() * Viewport.Y));
+            else if (StartPlace < 1.0)
+                bullet.Position = new Vector2(Viewport.X, (float)(rng.NextDouble() * Viewport.Y));
+            GD.Print(bullet.Position);
+
+            double ArchTanAngle = Mathf.Atan2(
+                p.Position.Y - bullet.Position.Y,
+                p.Position.X - bullet.Position.X
+            );
+
+            bullet.Velocity = new Vector2(
+                (float)(Mathf.Cos(ArchTanAngle) * BulletSpeed),
+                (float)(Mathf.Sin(ArchTanAngle) * BulletSpeed)
+            );
+
+            bullet.Viewport = Viewport;
+            bullet.bulletSpeed = BulletSpeed;
+            bullet.p = p;
             GetTree().CurrentScene.AddChild(bullet);
         };
 
