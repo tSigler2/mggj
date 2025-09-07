a4b2b3a1bfd10d8073fbf4e583c31a253a6572f4
Kacy <kacywoodring@gmail.com>
2025-09-07 14:06:23 -0400

    Animation Stuffs

--- src/entity/NPC.cs
+++ src/entity/NPC.cs
@@ -2,37 +2,37 @@ using Godot;
 
 public partial class NPC : InteractableEntity
 {
-    [Export]
-    public string TargetBattleScenePath;
+	[Export]
+	public string TargetBattleScenePath;
 
-    [Export]
-    public string[] DialogueLines;
+	[Export]
+	public string[] DialogueLines;
 
-    private bool _hasInteracted = false;
+	private bool _hasInteracted = false;
 
-    public override void _Ready()
-    {
-        base._Ready();
-        OnInteract += OnNPCInteract;
-    }
+	public override void _Ready()
+	{
+		base._Ready();
+		OnInteract += OnNPCInteract;
+	}
 
-    private void OnNPCInteract()
-    {
-        if (_hasInteracted)
-            return;
+	private void OnNPCInteract()
+	{
+		if (_hasInteracted)
+			return;
 
-        GD.Print("Starting dialogue with NPC");
-        // TODO: Implement dialogue system
-        // For now, we'll just transition to the battle scene after a short delay
-        _hasInteracted = true;
+		GD.Print("Starting dialogue with NPC");
+		// TODO: Implement dialogue system
+		// For now, we'll just transition to the battle scene after a short delay
+		_hasInteracted = true;
 
-        GetTree().CreateTimer(1.0f).Timeout += () =>
-        {
-            if (!string.IsNullOrEmpty(TargetBattleScenePath))
-            {
-                GD.Print($"Loading battle scene: {TargetBattleScenePath}");
-                SceneManager.Instance.ChangeScene(TargetBattleScenePath);
-            }
-        };
-    }
+		GetTree().CreateTimer(1.0f).Timeout += () =>
+		{
+			if (!string.IsNullOrEmpty(TargetBattleScenePath))
+			{
+				GD.Print($"Loading battle scene: {TargetBattleScenePath}");
+				SceneManager.Instance.ChangeScene(TargetBattleScenePath);
+			}
+		};
+	}
 }
