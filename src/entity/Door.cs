4484e70790668fdcf1452de5fc2e29e70cc193e0
Oct56 <carsoncoles1105@gmail.com>
2025-09-07 23:58:06 -0400

    win/lose splash

--- src/entity/Door.cs
+++ src/entity/Door.cs
@@ -2,24 +2,24 @@ using Godot;
 
 public partial class Door : InteractableEntity
 {
-    [Export]
-    public string TargetScenePath;
+	[Export]
+	public string TargetScenePath;
 
-    [Export]
-    public Vector2 PlayerSpawnPosition;
+	[Export]
+	public Vector2 PlayerSpawnPosition;
 
-    public override void _Ready()
-    {
-        base._Ready();
-        OnInteract += OnDoorInteract;
-    }
+	public override void _Ready()
+	{
+		base._Ready();
+		OnInteract += OnDoorInteract;
+	}
 
-    private void OnDoorInteract()
-    {
-        if (!string.IsNullOrEmpty(TargetScenePath))
-        {
-            GD.Print($"Loading scene: {TargetScenePath}");
-            SceneManager.Instance.ChangeScene(TargetScenePath);
-        }
-    }
+	private void OnDoorInteract()
+	{
+		if (!string.IsNullOrEmpty(TargetScenePath))
+		{
+			GD.Print($"Loading scene: {TargetScenePath}");
+			SceneManager.Instance.ChangeScene(TargetScenePath);
+		}
+	}
 }
--- src/entity/NPC.cs
+++ src/entity/NPC.cs
@@ -3,51 +3,56 @@ using DialogueManagerRuntime;
 
 public partial class NPC : InteractableEntity
 {
-    [Export]
-    public string TargetBattleScenePath;
+	[Export]
+	public string TargetBattleScenePath;
 
-    [Export]
-    Resource dialogue = GD.Load<Resource>("");
+	[Export]
+	Resource dialogue = GD.Load<Resource>("");
 
-    [Export]
-    public string dialogueStart = "start";
+	[Export]
+	public string dialogueStart = "start";
 
-    private bool _hasInteracted = false;
+	[Export]
+	Player player;
 
-    public override void _Ready()
-    {
-        base._Ready();
-        OnInteract += OnNPCInteract;
-    }
+	private bool _hasInteracted = false;
 
-    private void OnNPCInteract()
-    {
-        if (_hasInteracted)
-        {
-            return;
-        }
-        // TODO: Implement dialogue system
-        // For now, we'll just transition to the battle scene after a short delay
+	public override void _Ready()
+	{
+		base._Ready();
+		OnInteract += OnNPCInteract;
+	}
 
-        _hasInteracted = true;
-        GD.Print("Starting dialogue with NPC");
-        DialogueManager.ShowDialogueBalloon(dialogue, dialogueStart);
-        DialogueManager.DialogueEnded += (Resource dialogueResource) =>
-        {
-            if (!string.IsNullOrEmpty(TargetBattleScenePath))
-            {
-                GD.Print($"Loading battle scene: {TargetBattleScenePath}");
-                SceneManager.Instance.ChangeScene(TargetBattleScenePath);
-            }
-        };
+	private void OnNPCInteract()
+	{
+		if (_hasInteracted)
+		{
+			return;
+		}
+		// TODO: Implement dialogue system
+		// For now, we'll just transition to the battle scene after a short delay
 
-        /*GetTree().CreateTimer(1.0f).Timeout += () =>
-        {
-            if (!string.IsNullOrEmpty(TargetBattleScenePath))
-            {
-                GD.Print($"Loading battle scene: {TargetBattleScenePath}");
-                SceneManager.Instance.ChangeScene(TargetBattleScenePath);
-            }
-        };*/
-    }
+		_hasInteracted = true;
+		//player.CanMove = false;
+		GD.Print("Starting dialogue with NPC");
+		DialogueManager.ShowDialogueBalloon(dialogue, dialogueStart);
+		DialogueManager.DialogueEnded += (Resource dialogueResource) =>
+		{
+			if (!string.IsNullOrEmpty(TargetBattleScenePath))
+			{
+				GD.Print($"Loading battle scene: {TargetBattleScenePath}");
+				//player.CanMove = true;
+				SceneManager.Instance.ChangeScene(TargetBattleScenePath);
+			}
+		};
+
+		/*GetTree().CreateTimer(1.0f).Timeout += () =>
+		{
+			if (!string.IsNullOrEmpty(TargetBattleScenePath))
+			{
+				GD.Print($"Loading battle scene: {TargetBattleScenePath}");
+				SceneManager.Instance.ChangeScene(TargetBattleScenePath);
+			}
+		};*/
+	}
 }
